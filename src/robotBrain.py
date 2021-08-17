6a4732005dce24f08861a5f37bda41522070c028
SantiagoDoti <santiagodoti@eudaimonia.com.ar>
2021-08-16 23:54:46 -0300

    Code refactor (WIP)

--- src/robotBrain.py
+++ src/robotBrain.py
@@ -1,59 +1,55 @@
-import cv2, time, numpy as np
+import cv2
+import numpy as np
 
-# Colores para filtrar
-low_white = np.array([5,5,160])
-up_white = np.array([179,85,235])
-low_yellow = np.array([18,94,140])
-up_yellow = np.array([48,255,255])
+# Filter colors
+low_white = np.array([5, 5, 160])
+up_white = np.array([179, 85, 235])
+low_yellow = np.array([18, 94, 140])
+up_yellow = np.array([48, 255, 255])
 
-def interestRegion(img, vertices):
+
+def regionOfInterest(img):
+    height = frame.shape[0]
+    width = frame.shape[1]
+    triangle = [np.array([(150, height), (850, height), (450, 320)])]
+    trapeze = [np.array([(0, height), (width / 2, height / 2), (width, height)], dtype=np.int32)]
     mask = np.zeros_like(img)
-    ignoreMaskColor = 255
-    cv2.fillPoly(mask,vertices,ignoreMaskColor)
-    maskedImage = cv2.bitwise_and(img,mask)
-    return maskedImage
+    cv2.fillPoly(mask, trapeze, 255)
+    return cv2.bitwise_and(img, mask)
+
 
 def drawLines(img):
-    lines = cv2.HoughLinesP(img,1, np.pi/180,threshold=50, maxLineGap=50)
+    lines = cv2.HoughLinesP(img, rho=1, theta=np.pi / 180, threshold=50, minLineLength=100, maxLineGap=50)
     if lines is not None:
         for line in lines:
-            x1,y1,x2,y2 = line[0]
-            cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),3)
+            x1, y1, x2, y2 = line[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
 
 def processImage(img):
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    img = cv2.GaussianBlur(img,(5,5),0)
-    mask = cv2.inRange(img,low_white,up_white)
-    finalImage = cv2.Canny(mask,75,150)
-    return finalImage
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    mask = cv2.inRange(img, low_white, up_white)
+    return cv2.Canny(mask, 75, 150)
+
 
 # Video en directo de la webcam
 video = cv2.VideoCapture(0)
 
 while True:
-
     check, frame = video.read()
 
     processedImage = processImage(frame)
-        
-    # Vertices del ROI (region of interest)
-    height = frame.shape[0]
-    widht = frame.shape[1]
-    verticesRegionInterest = [np.array([(0,height),(widht/2,height/2),(widht,height)],dtype=np.int32)]
-    
-    croppedImage = interestRegion(processedImage,verticesRegionInterest)
+
+    croppedImage = regionOfInterest(processedImage)
     drawLines(croppedImage)
 
-    cv2.imshow("ROBOT VISION",frame)
-    #cv2.imshow("CROPPED IMAGE",croppedImage)
-    #cv2.imshow("PROCESSED IMAGE",processedImage)
+    cv2.imshow("ROBOT VISION", frame)
+    # cv2.imshow("CROPPED IMAGE",croppedImage)
+    # cv2.imshow("PROCESSED IMAGE",processedImage)
 
-    key = cv2.waitKey(1)
-    if key == 27:
+    if cv2.waitKey(1) == 27:
         break
 
 video.release()
 cv2.destroyAllWindows()
-
-
-    
--- src/videoTest.py
+++ src/videoTest.py
@@ -1,64 +1,68 @@
-import cv2, time, numpy as np
 import os.path
 
+import cv2
+import numpy as np
+
 rootPath = os.path.abspath(os.path.dirname(__file__))
-videoPath = os.path.join(rootPath, "../tests/testYellowWhiteRight.mp4")
+videoPath = os.path.join(rootPath, "../tests/testWhiteRight.mp4")
 
-# Colores blancos para filtrar
-low_white = np.array([5,5,160])
-up_white = np.array([179,85,235])
-low_yellow = np.array([18,94,140])
-up_yellow = np.array([48,255,255])
+# Filter colors
+low_white = np.array([5, 5, 160])
+up_white = np.array([179, 85, 235])
+low_yellow = np.array([18, 94, 140])
+up_yellow = np.array([48, 255, 255])
 
 
-def interestRegion(img, vertices):
+def regionOfInterest(img):
+    height = frame.shape[0]
+    width = frame.shape[1]
+    triangle = [np.array([(150, height), (850, height), (450, 320)])]
+    trapeze = [np.array([(0, height), (width / 2, height / 2), (width, height)], dtype=np.int32)]
     mask = np.zeros_like(img)
-    ignoreMaskColor = 255
-    cv2.fillPoly(mask,vertices,ignoreMaskColor)
-    maskedImage = cv2.bitwise_and(img,mask)
-    return maskedImage
+    cv2.fillPoly(mask, trapeze, 255)
+    return cv2.bitwise_and(img, mask)
+
 
 def drawLines(img):
-    lines = cv2.HoughLinesP(img,rho=1, theta=np.pi/180,threshold=50, minLineLength=70 ,maxLineGap=10)
+    lines = cv2.HoughLinesP(img, rho=1, theta=np.pi/180, threshold=50, minLineLength=70, maxLineGap=20)
     if lines is not None:
         for line in lines:
-            x1,y1,x2,y2 = line[0]
-            cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),3)
+            x1, y1, x2, y2 = line[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
 
 def processImage(img):
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    img = cv2.GaussianBlur(img,(5,5),0)
-    mask = cv2.inRange(img,low_white,up_white)
-    finalImage = cv2.Canny(mask,75,150)
-    return finalImage
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    mask = cv2.inRange(img, low_white, up_white)
+    return cv2.Canny(mask, 75, 150)
+
 
 # Video pregrabado
 video = cv2.VideoCapture(videoPath)
 
 pos_frame = video.get(cv2.CAP_PROP_POS_FRAMES)
+
 while True:
     flag, frame = video.read()
-    if flag:
-        processedImage = processImage(frame)
 
-        # Vertices del ROI (region of interest)
-        height = frame.shape[0]
-        widht = frame.shape[1]
-        verticesRegionInterest = [np.array([(0,height),(widht/2,height/2),(widht,height)],dtype=np.int32)]
-        
-        croppedImage = interestRegion(processedImage,verticesRegionInterest)
-        drawLines(croppedImage)
+    # Loop video
+    if not flag:
+        video = cv2.VideoCapture(videoPath)
+        continue
 
-        cv2.imshow("Video pregrabado",frame)
-        pos_frame = video.get(cv2.CAP_PROP_POS_FRAMES)
-        print(str(pos_frame)+ " frames y " +str(video.get(cv2.CAP_PROP_POS_MSEC)/1000) +" segundos")
-    else:
-        # Reinicio video para que se reproduzca en loop
-        print("<---------------------- REINICIANDO VIDEO ---------------------->")
-        video.set(cv2.CAP_PROP_POS_FRAMES,0)
+    processedImage = processImage(frame)
+
+    croppedImage = regionOfInterest(processedImage)
+    drawLines(croppedImage)
+
+    cv2.imshow("Video pregrabado", frame)
+    cv2.imshow("Cropped image", croppedImage)
+    pos_frame = video.get(cv2.CAP_PROP_POS_FRAMES)
+    # print(str(pos_frame) + " frames y " + str(video.get(cv2.CAP_PROP_POS_MSEC)/1000) + " segundos")
+
     if cv2.waitKey(10) == 27:
         break
-    if video.get(cv2.CAP_PROP_POS_FRAMES) == video.get(cv2.CAP_PROP_FRAME_COUNT):
-        break
 
+video.release()
 cv2.destroyAllWindows()
